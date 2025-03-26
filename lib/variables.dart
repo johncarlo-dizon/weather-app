b3800223bed52b9fcec19019a0ac6ae96b7f0578
johncarlo-dizon <johncarlovdizon@gmail.com>
2025-03-26 16:27:09 +0800

    variables

--- lib/variables.dart
+++ lib/variables.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/cupertino.dart';
 
+String citylocation = "Manila";
+Map<String, dynamic> weatherData = {};
+String url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    citylocation +
+    "&appid=05f53c8bfd0bf75b9bf78e2edf3abfff";
+String city = "Loading...";
+
+String temperature = "°°°°";
+String humidity = "°°°°";
+String feels = "°°°°";
+String visibility = "°°°°";
+String pressure = "°°°°";
+String cloud = "°°°°";
+String wind = "°°°°";
+int windDegree = 0;
+String windDirection = "N";
+Color colorIcon = CupertinoColors.systemPurple;
+Brightness mode = Brightness.dark;
+bool isDark = true;
+bool showDeveloper = false;
+Color darkColor = CupertinoColors.white;
+
+bool isCelsius = true;
