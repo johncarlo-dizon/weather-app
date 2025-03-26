3c6e3380bfd418a15aee434193a88ac1a4519885
johncarlo-dizon <johncarlovdizon@gmail.com>
2025-03-26 16:25:20 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,210 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'variables.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/settings.dart';
+
+void main() {
+  runApp(MyApp()); // Wrap everything in the main widget
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Brightness mode = Brightness.dark;
+
+  Map<String, dynamic> weatherData = {};
+  String url = "";
+  String city = "Loading...";
+  IconData? weather;
+  bool newMetric = true;
+  double tempInCelsius = 0.0;
+  double feelslike = 0.0;
+  bool dataload = false;
+
+  void updateLocation(String newLocation) {
+    setState(() {
+      citylocation = newLocation;
+      url =
+          "https://api.openweathermap.org/data/2.5/weather?q=$citylocation&appid=05f53c8bfd0bf75b9bf78e2edf3abfff";
+      getData();
+    });
+  }
+
+  void updateColor(Color newColor) {
+    setState(() {
+      colorIcon = newColor;
+    });
+  }
+
+  void updateMetric(bool newValue) {
+    setState(() {
+      newMetric = newValue; // Update the value of Celsius/Fahrenheit
+      print(newMetric);
+    });
+  }
+
+  void updateMode(Brightness newMode) {
+    setState(() {
+      mode = newMode;
+    });
+  }
+
+  Future<void> getData() async {
+    final response = await http.get(Uri.parse(url));
+
+    setState(() {
+      weatherData = jsonDecode(response.body);
+      String cod = (weatherData["cod"]).toString();
+
+      if (cod == "404") {
+        setState(() {
+          dataload = false;
+        });
+        navigatorKey.currentState?.pushReplacement(
+          CupertinoPageRoute(
+            builder: (context) {
+              return CupertinoAlertDialog(
+                title: Text(
+                  "Invalid Location",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                ),
+                content: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Text(
+                    "The location you entered is invalid. Please try entering a valid location.",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: CupertinoColors.inactiveGray,
+                    ),
+                  ),
+                ),
+                actions: [
+                  CupertinoButton(
+                    child: Text(
+                      "Close",
+                      style: TextStyle(color: CupertinoColors.destructiveRed),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
+        );
+      } else {
+        city = weatherData["name"];
+        dataload = true;
+        tempInCelsius = weatherData["main"]["temp"] - 273.15;
+        feelslike = weatherData["main"]["feels_like"] - 273.15;
+
+        if (newMetric) {
+          // For Celsius
+          temperature = tempInCelsius.toStringAsFixed(1) + "°C";
+          feels = feelslike.toStringAsFixed(1) + "°C";
+        } else {
+          // For Fahrenheit
+          temperature =
+              ((tempInCelsius * 9 / 5) + 32).toStringAsFixed(1) + "°F";
+          feels = ((feelslike * 9 / 5) + 32).toStringAsFixed(1) + "°F";
+        }
+
+        humidity = (weatherData["main"]["humidity"]).toString() + "%";
+        pressure = (weatherData["main"]["pressure"]).toString() + " hPa";
+        visibility = (weatherData["visibility"] / 1000).toString() + " km";
+        cloud = (weatherData["clouds"]["all"]).toString() + "%";
+        wind =
+            (weatherData["wind"]["speed"] * 3.6).toStringAsFixed(1) + " km/h";
+        windDegree = weatherData["wind"]["deg"];
+
+        if (windDegree >= 0 && windDegree < 22.5) {
+          windDirection = "N";
+        } else if (windDegree >= 22.5 && windDegree < 67.5) {
+          windDirection = "NE";
+        } else if (windDegree >= 67.5 && windDegree < 112.5) {
+          windDirection = "E";
+        } else if (windDegree >= 112.5 && windDegree < 157.5) {
+          windDirection = "SE";
+        } else if (windDegree >= 157.5 && windDegree < 202.5) {
+          windDirection = "S";
+        } else if (windDegree >= 202.5 && windDegree < 247.5) {
+          windDirection = "SW";
+        } else if (windDegree >= 247.5 && windDegree < 292.5) {
+          windDirection = "W";
+        } else if (windDegree >= 292.5 && windDegree < 337.5) {
+          windDirection = "NW";
+        } else {
+          windDirection = "N";
+        }
+
+        switch (weatherData["weather"][0]["main"]) {
+          case "Clouds":
+            weather = CupertinoIcons.cloud;
+            break;
+          case "Clear":
+            weather = CupertinoIcons.sun_max;
+            break;
+          case "Mist":
+            weather = CupertinoIcons.cloud_rain;
+            break;
+          case "Rain":
+            weather = CupertinoIcons.cloud_bolt_rain;
+            break;
+          case "Snow":
+            weather = CupertinoIcons.snow;
+            break;
+          case "Thunderstorm":
+            weather = CupertinoIcons.cloud_bolt;
+            break;
+          case "Drizzle":
+            weather = CupertinoIcons.cloud_drizzle;
+            break;
+          default:
+            weather = CupertinoIcons.cloud;
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    url =
+        "https://api.openweathermap.org/data/2.5/weather?q=$citylocation&appid=05f53c8bfd0bf75b9bf78e2edf3abfff";
+    getData();
+  }
+
+  GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  @override
+  Widget build(BuildContext context) {
+    if (dataload == true) {
+      if (newMetric) {
+        temperature = tempInCelsius.toStringAsFixed(1) + "°C";
+        feels = feelslike.toStringAsFixed(1) + "°C";
+      } else {
+        temperature = ((tempInCelsius * 9 / 5) + 32).toStringAsFixed(1) + "°F";
+        feels = ((feelslike * 9 / 5) + 32).toStringAsFixed(1) + "°F";
+      }
+    } else {
+      city = "Loading...";
+      temperature = "°°°°";
+      humidity = "°°°°";
+      feels = "°°°°";
+      visibility = "°°°°";
+      pressure = "°°°°";
+      cloud = "°°°°";
+      wind = "°°°°";
+    }
+
+    return CupertinoApp();
+  }
+}
